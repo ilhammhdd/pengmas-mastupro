7ff1fd11b2f586ce9bf7871337b4a23a70f4aab6
ilhammhdd <milham939@gmail.com>
2017-12-15 09:14:12 +0700

    folder cleaned

--- resources/views/include/website-loading.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Website Loading
-============================================= -->	
-<div id="website-loading">
-	<div class="loader">
-		<span class="heart-beam"></span>
-	</div><!-- .loader end -->
-</div><!-- .website-loading end -->
