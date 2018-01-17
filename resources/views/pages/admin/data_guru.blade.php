87233c0773d6edd222478b7e05bcc38f0cbe0a9f
abdulaziz733 <abdulaziz733@gmail.com>
2018-01-17 22:15:04 +0700

    revisi v1.0.2

--- resources/views/pages/admin/data_guru.blade.php
+++ resources/views/pages/admin/data_guru.blade.php
@@ -179,7 +179,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="myModalLabel">Reset Password</h4>
+            <h4 class="modal-title" id="myModalLabel">Reset Akun</h4>
           </div>
           <div class="modal-body">
             <form id="rpFormValidation" action="{{route('admin.reset_password_guru')}}" method="post" novalidate="">
